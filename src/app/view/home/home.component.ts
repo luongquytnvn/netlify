b9f64a85bd3300f532fdd21423bd5b73dc2ddc83
luongquy <quylv@itsol.vn>
2020-09-05 12:00:13 +0700

    update

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -18,14 +18,15 @@ export class HomeComponent implements OnInit {
   outerWidth: BehaviorSubject<any> = new BehaviorSubject<any>(0);
   outerHeight: BehaviorSubject<any> = new BehaviorSubject<any>(0);
   documentHeight: BehaviorSubject<any> = new BehaviorSubject<any>(0);
+  heightCustom: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
   constructor() {
   }
 
   ngOnInit(): void {
-    // $(window).resize(() => {
-    //   this.resize();
-    // });
+    $(window).resize(() => {
+      this.resize();
+    });
   }
 
   resize() {
@@ -37,6 +38,15 @@ export class HomeComponent implements OnInit {
     this.availWidth.next(screen.availWidth);
     this.outerWidth.next(window.outerWidth);
     this.outerHeight.next(window.outerHeight);
+
+    console.log('screenHeight', this.screenHeight.getValue());
+    console.log('screenWidth', this.screenWidth.getValue());
+    console.log('innerHeight', this.innerHeight.getValue());
+    console.log('innerWidth', this.innerWidth.getValue());
+    console.log('availHeight', this.availHeight.getValue());
+    console.log('availWidth', this.availWidth.getValue());
+    console.log('outerWidth', this.outerWidth.getValue());
+    console.log('outerHeight', this.outerHeight.getValue());
     //   $(document).ready(() => {
     //     const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     //     const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
@@ -47,23 +57,24 @@ export class HomeComponent implements OnInit {
 
     $(document).ready(() => {
       this.documentHeight.next($(document).height());
-      const bs1 = $('.bs-background');
-      const bs2 = $('.bs-background-form');
-      const bs3 = $('.sb-form');
-      bs1.removeClass('bs-background-mobile');
-      bs2.removeClass('bs-background-form-mobile');
-      bs3.removeClass('bs-form-mobile');
+      const bs1 = $('.bg-container');
+      const bs2 = $('.bg-login');
+      const bs3 = $('.sb-login-custom-form');
+      // bs1.removeClass('bs-background-mobile');
+      // bs2.removeClass('bs-background-form-mobile');
+      // bs3.removeClass('bs-form-mobile');
       if ((innerHeight < (screen.height * 90 / 100)) && (screen.width <= 575)) {
         if (screen.width < screen.height) {
-          bs1.addClass('bs-background-mobile');
-          bs2.addClass('bs-background-form-mobile');
-          bs3.addClass('bs-form-mobile');
-          const bs4 = $('.bs-background-form-mobile');
-          const bs5 = $('.bs-form-mobile');
-          const bs6 = $('.bs-background-mobile');
-          bs4.height(screen.height);
-          bs5.height(screen.height);
-          bs6.find('*').css('font-size', screen.height * 1.8 / 100);
+          // bs1.addClass('bs-background-mobile');
+          // bs2.addClass('bs-background-form-mobile');
+          // bs3.addClass('bs-form-mobile');
+          // const bs4 = $('.bs-background-form-mobile');
+          // const bs5 = $('.bs-form-mobile');
+          // const bs6 = $('.bs-background-mobile');
+          // bs4.height(screen.height);
+          // bs5.height(screen.height);
+          // bs6.find('*').css('font-size', screen.height * 1.8 / 100);
+          this.heightCustom.next(screen.height);
         }
       }
     });
